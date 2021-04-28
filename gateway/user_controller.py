6b7d7baad322eb21f99288776395f588151b66ae
bz <bzx@tuta.io>
2021-04-28 20:17:35 -0300

    user controller send message method created

--- gateway/user_controller.py
+++ gateway/user_controller.py
@@ -1,4 +1,4 @@
-from flask import json
+from flask import json, request
 from flask_restful import Resource
 
 from meta.meta import MetaInfo
@@ -29,3 +29,55 @@ class GetMe(Resource):
         except Exception as e:
             return str(e.__repr__()), 400
 
+
+class UserMessages(Resource):
+    def post(self):
+        """
+Send message to someone
+---
+parameters:
+  - in: body
+    name: body
+    description: JSON parameters.
+    required: true
+    id: Message
+    schema:
+      properties:
+        to:
+          type: string
+          description: Username.
+          example: telegram
+        text:
+          type: string
+          description: Message text.
+          example: Hello World!
+responses:
+  201:
+    description: Sended data
+    schema:
+      properties:
+        message:
+          type: string
+          description: Text sended.
+          example: Hello World!
+        info:
+          type: object
+          description: Sended message information
+    id: MessageSend
+  400:
+    description: Error sending message
+        """
+        try:
+            # Get message JSON
+            content = request.get_json(force=True)
+
+            # Parse JSON
+            send_to = content['to']
+            message = content['text']
+
+            # Send user message
+            message_info = MetaInfo.client.send_message(f"{send_to}", f"{message}")
+
+            return {'message': message, 'info': json.loads(str(message_info))}, 201
+        except Exception as e:
+            return str(e.__repr__()), 400
