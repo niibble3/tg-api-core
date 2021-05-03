3c6a09a4c9ce8d3c5c3317639941c81830880e5b
bz <bzx@tuta.io>
2021-05-03 20:22:43 -0300

    participantController query schema

--- schemas/api.py
+++ schemas/api.py
@@ -5,3 +5,8 @@ class ChatControllerSchema(Schema):
     limit = fields.Int(required=False, default=10)
     pinned_only = fields.Bool(required=False)
 
+
+class ParticipantControllerGetSchema(Schema):
+    user = fields.Str(required=True)
+    users = fields.List(fields.Str(), required=False)
+
