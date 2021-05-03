30646e6a3db1f536b37555684aa10badba284c2b
bz <bzx@tuta.io>
2021-05-03 17:23:17 -0300

    implemented limit and pinned_only in get_dialogs

--- gateway/chat_controller.py
+++ gateway/chat_controller.py
@@ -1,14 +1,28 @@
 from flask import json, request
 from flask_restful import Resource
 
+from schemas.api import ChatControllerSchema
+
 from meta.meta import MetaInfo
 
 
 class ChatController(Resource):
     def get(self):
+        arg_schema = ChatControllerSchema()
         try:
+            # Validate args
+            args = request.args
+            err = arg_schema.validate(args)
+            if err:
+                raise NotImplementedError('invalid arguments')
+
+            # total chats returned, default 10
+            limit = args['limit'] if 'limit' in args else 10
+            # pinned chats only, default False
+            pinned = args['pinned_only'] if 'pinned_only' in args else False
+
             total_chats = MetaInfo.client.get_dialogs_count()
-            chat_list = MetaInfo.client.get_dialogs()
+            chat_list = MetaInfo.client.get_dialogs(limit=int(limit), pinned_only=bool(pinned))
 
             return {'chats': json.loads(str(chat_list)), 'total': total_chats}, 200
         except Exception as e:
